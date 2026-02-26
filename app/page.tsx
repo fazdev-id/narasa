e71b1d5361552fc8ad8a34ec14599c665ca9cfdc
boneless <boneless@boneless.boneless>
2026-02-26 13:48:58 +0800

    Initial commit: NARASA Under Construction

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>Selamat datang di NARASA - Under Construction</h1>
+    </div>
+  )
+}
